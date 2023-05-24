cddd9808c9a23d7303e35042ff0592686472e976
Uigorteodoro <uigorgit32@gmail.com>
2023-05-24 13:29:46 -0300

    Inicio e conclusão da aula 15

---
+++ Aula 15-Curso-PHP/Rotina externa/02exercicio.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="../_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+  <?php
+    require_once 'funcoes.php';
+    echo "<h1>Testando funções em PHP</h1>";
+    ola();
+    retornavalor(8/2+850);
+  ?>
+</div>
+</body>
+</html>
+ 
---
+++ Aula 15-Curso-PHP/Rotina externa/funcoes.php
@@ -0,0 +1,10 @@
+<?php
+    function ola(){
+        echo "<h1>Olá galera</h1>";
+    }
+
+    function retornavalor($v){
+        $s = "<span style='color:red;'>$v</span>";
+        echo "<h2>Acabei de receber o valor passado de V que é: $s</h2>";
+    }
+?>
