691d8132b71c01cae9628e8743f3aff66aed295e
Uigorteodoro <uigorgit32@gmail.com>
2023-05-24 13:28:06 -0300

    Conclusão da Aula 12

--- Aula 12-Curso-PHP/01-contador.php
+++ Aula 12-Curso-PHP/01-contador.php
@@ -7,7 +7,14 @@
 </head>
 <body>
 <div>
-  
+    <?php
+        $c = 1;
+        do {
+            echo "$c ";
+            $c++;
+        } while($c<=10);
+        
+    ?>
 </div>
 </body>
 </html>
